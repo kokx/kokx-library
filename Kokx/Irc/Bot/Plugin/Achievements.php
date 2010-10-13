2f7c27fd653c2fa4eb6e1d23f0eb45a9d369c425
Pieter Kokx <pieter@kokx.nl>
2010-10-13 11:35:35 +0200

    Added basic functionality for confirming users with NickServ.

--- Kokx/Irc/Bot/Plugin/Achievements.php
+++ Kokx/Irc/Bot/Plugin/Achievements.php
@@ -29,6 +29,20 @@ class Kokx_Irc_Bot_Plugin_Achievements implements Kokx_Irc_Bot_Plugin_PluginInte
      */
     protected $_config;
 
+    /**
+     * Confirmed users
+     *
+     * @var array
+     */
+    protected $_confirmed = array();
+
+    /**
+     * Client
+     *
+     * @var Kokx_Irc_Client
+     */
+    protected $_client;
+
 
     /**
      * Constructor
@@ -40,6 +54,10 @@ class Kokx_Irc_Bot_Plugin_Achievements implements Kokx_Irc_Bot_Plugin_PluginInte
     public function __construct(array $config = array())
     {
         $this->_config = $config;
+
+        if (!isset($this->_config['NickServ'])) {
+            $this->_config['NickServ'] = 'NickServ';
+        }
     }
 
     /**
@@ -62,6 +80,64 @@ class Kokx_Irc_Bot_Plugin_Achievements implements Kokx_Irc_Bot_Plugin_PluginInte
     public function register(Kokx_Event_Dispatcher $dispatcher)
     {
         $dispatcher->subscribe('privmsg', array($this, 'privmsg'));
+
+        $dispatcher->subscribe('part', array($this, 'part'));
+        $dispatcher->subscribe('quit', array($this, 'part'));
+
+        $dispatcher->subscribe('join', array($this, 'join'));
+        $dispatcher->subscribe('353', array($this, 'names'));
+    }
+
+    /**
+     * PART/QUIT event
+     *
+     * @param Kokx_Event $event
+     *
+     * @return void
+     */
+    public function part(Kokx_Event $event)
+    {
+        unset($this->_confirmed[$event['nick']]);
+    }
+
+    /**
+     * JOIN event
+     *
+     * @param Kokx_Event $event
+     *
+     * @return void
+     */
+    public function join(Kokx_Event $event)
+    {
+        $this->_client = $event->getSubject();
+
+        $this->_confirm($event['nick']);
+    }
+
+    /**
+     * NAMES event
+     *
+     * @param Kokx_Event $event
+     *
+     * @return void
+     */
+    public function names(Kokx_Event $event)
+    {
+        $this->_client = $event->getSubject();
+
+        $names = explode(' ', $event['message']);
+
+        foreach ($names as $nick) {
+            switch ($nick[0]) {
+                case '@':
+                case '&':
+                case '%':
+                case '+':
+                    $nick = substr($nick, 1);
+                default:
+                    $this->_confirm($nick);
+            }
+        }
     }
 
     /**
@@ -73,10 +149,31 @@ class Kokx_Irc_Bot_Plugin_Achievements implements Kokx_Irc_Bot_Plugin_PluginInte
      */
     public function privmsg(Kokx_Event $event)
     {
-        $matches = array();
+        $this->_client = $event->getSubject();
 
-        if (($event['nick'] == $this->_config['NickServ']['name'])
-        && ($event['host'] == $this->_config['NickServ']['host'])) {
+        if ($event['nick'] == $this->_config['NickServ']) {
+            $matches = array();
+            // we were busy confirming a nick
+            if (preg_match('/Nickname: (?<nick>[a-zA-Z]([a-zA-Z0-9{}\[\]\\`^-])) << ONLINE >>/i', $event['message'], $matches)) {
+                if (isset($this->_confirmed[$matches['nick']])) {
+                    $this->_confirmed[$matches['nick']] = true;
+                }
+            }
         }
+    }
+
+    /**
+     * Confirm a user
+     *
+     * @param string $nick
+     *
+     * @return void
+     */
+    protected function _confirm($nick)
+    {
+        $this->_confirmed[$nick] = false;
+
+        // ask confirmation to NickServ
+        $this->_client->send('info ' . $nick, $this->_config['NickServ']);
     }
 }
